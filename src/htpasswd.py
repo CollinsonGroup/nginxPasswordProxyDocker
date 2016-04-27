84723c8387d7b15a909543ea1a34a62aafe1ec6c
Matt Kimber <matt.kimber@collinsongroup.com>
2016-04-27 11:11:02 +0000

    use env vars in Python script directly rather than shell

--- src/htpasswd.py
+++ src/htpasswd.py
@@ -22,7 +22,7 @@ except ImportError:
 
 
 def salt():
-    """Returns a string of 2 randome letters"""
+    """Returns a string of 2 random letters"""
     letters = 'abcdefghijklmnopqrstuvwxyz' \
               'ABCDEFGHIJKLMNOPQRSTUVWXYZ' \
               '0123456789/.'
@@ -32,23 +32,9 @@ def salt():
 class HtpasswdFile:
     """A class for manipulating htpasswd files."""
 
-    def __init__(self, filename, create=False):
+    def __init__(self, filename):
         self.entries = []
         self.filename = filename
-        if not create:
-            if os.path.exists(self.filename):
-                self.load()
-            else:
-                raise Exception("%s does not exist" % self.filename)
-
-    def load(self):
-        """Read the htpasswd file into memory."""
-        lines = open(self.filename, 'r').readlines()
-        self.entries = []
-        for line in lines:
-            username, pwhash = line.split(':')
-            entry = [username, pwhash.rstrip()]
-            self.entries.append(entry)
 
     def save(self):
         """Write the htpasswd file to disk"""
@@ -58,31 +44,14 @@ class HtpasswdFile:
     def update(self, username, password):
         """Replace the entry for the given user, or add it if new."""
         pwhash = crypt.crypt(password, salt())
-        matching_entries = [entry for entry in self.entries
-                            if entry[0] == username]
-        if matching_entries:
-            matching_entries[0][1] = pwhash
-        else:
-            self.entries.append([username, pwhash])
-
-    def delete(self, username):
-        """Remove the entry for the given user."""
-        self.entries = [entry for entry in self.entries
-                        if entry[0] != username]
+        self.entries.append([username, pwhash])
 
 
 def main():
-    """%prog [-c] -b filename username password
-    Create or update an htpasswd file"""
+    """%prog filename
+    Create an htpasswd file"""
     # For now, we only care about the use cases that affect tests/functional.py
     parser = OptionParser(usage=main.__doc__)
-    parser.add_option('-b', action='store_true', dest='batch', default=False,
-        help='Batch mode; password is passed on the command line IN THE CLEAR.'
-        )
-    parser.add_option('-c', action='store_true', dest='create', default=False,
-        help='Create a new htpasswd file, overwriting any existing file.')
-    parser.add_option('-D', action='store_true', dest='delete_user',
-        default=False, help='Remove the given user from the password file.')
 
     options, args = parser.parse_args()
 
@@ -94,28 +63,17 @@ def main():
         sys.stderr.write(parser.get_usage())
         sys.exit(1)
 
-    if not options.batch:
-        syntax_error("Only batch mode is supported\n")
-
     # Non-option arguments
-    if len(args) < 2:
+    if len(args) < 1:
         syntax_error("Insufficient number of arguments.\n")
-    filename, username = args[:2]
-    if options.delete_user:
-        if len(args) != 2:
-            syntax_error("Incorrect number of arguments.\n")
-        password = None
-    else:
-        if len(args) != 3:
-            syntax_error("Incorrect number of arguments.\n")
-        password = args[2]
+    filename = args[0]
 
-    passwdfile = HtpasswdFile(filename, create=options.create)
+    passwdfile = HtpasswdFile(filename)
 
-    if options.delete_user:
-        passwdfile.delete(username)
-    else:
-        passwdfile.update(username, password)
+    username = os.environ['PASSTHRU_USERNAME']
+    password = os.environ['PASSTHRU_PASSWORD']
+
+    passwdfile.update(username, password)
 
     passwdfile.save()
 
--- src/start.sh
+++ src/start.sh
@@ -4,6 +4,6 @@ echo Configuring nginx
 python /configScripts/createNginxConfig.py
 
 echo Setting Password
-python /configScripts/htpasswd.py -c -b /etc/nginx/htpasswd "$PASSTHRU_USERNAME" "$PASSTHRU_PASSWORD"
+python /configScripts/htpasswd.py /etc/nginx/htpasswd
 
 exec "$@"
