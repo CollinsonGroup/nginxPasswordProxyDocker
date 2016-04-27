4e86ff6bc86acfb43754f8de1abb307bb6f20da4
Matt Kimber <matt.kimber@collinsongroup.com>
2016-04-27 10:43:26 +0000

    Remove debug print commands

--- src/start.sh
+++ src/start.sh
@@ -3,9 +3,6 @@ set -e
 echo Configuring nginx
 python /configScripts/createNginxConfig.py
 
-echo '$PASSTHRU_USERNAME'
-echo '$PASSTHRU_PASSWORD'
-
 echo Setting Password
 python /configScripts/htpasswd.py -c -b /etc/nginx/htpasswd "$PASSTHRU_USERNAME" "$PASSTHRU_PASSWORD"
 
